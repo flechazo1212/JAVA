00a1d64f4bd69486b29e6dd1e1149336a53e90a3
ductoan1212 <148407817+flechazo1212@users.noreply.github.com>
2024-10-23 20:34:15 +0700

    Student.java

--- Student.java
+++ Student.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-import java.util.Arrays;
 
 class Student{
     private String name;
@@ -31,45 +29,6 @@ class Student{
     public void printStudent(){
         System.out.print("name: " + name + " score: " + score);
    }
-
 }
 
-public class StudentTest{
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("enter student number: ");
-        int n = s.nextInt()();
-        String[] Student = new Student();
-
-    }
-
-    public static void inputStudents(Student[] students, Scanner s){
-        for(int i = 0; i < students.length; i++){
-            System.out.println("enter student name " + (i + 1) + ": ");
-            String name = s.nextLine();
-        }
-        for(int i = 0; i < students.length;i++){
-            System.out.println("enter student score " + (i + 1) + ": ");
-            int score = s.nextInt();
-        }
-    }
-
-    public static void printStudents(Student[] students) {
-        for(int i = 0; i < students.length; i++) {
-            students[i].printStudent();
-        }
-    }
-
-    public static void sortScores(Student[] students) {
-        for(int i = 0; i < students.length - 1; i++) {
-            for(int j = i + 1; j < students.length; j++) {
-                if (students[i].getScore() < students[j].getScore()) {
-                    Student temp = students[i];
-                    students[i] = students[j];
-                    students[j] = temp;
-                }
-            }
-        }
-    }
-}
 
